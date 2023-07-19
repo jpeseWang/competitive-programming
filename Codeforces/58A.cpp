b744e7c3058bfef55329ae1391e6fb45de36c0e7
wang <quang07112004@gmail.com>
2023-07-19 23:00:55 +0700

    first commit

---
+++ Codeforces/58A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string s; cin >> s;
+    string k="hello";
+    int j(0), count(0);
+    for (int i=0; i<s.length();i++){
+        if(s[i]==k[j]){
+            j++;
+            count++;
+            if(count==5) break;
+        }
+    }
+    if (count ==5) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
